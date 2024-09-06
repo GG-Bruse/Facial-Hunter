7febfa86f2ec7286e13659de807f9003aa2d18c5
GG-Bruse <BJY811@outlook.com>
2024-09-06 14:13:35 +0800

    moveCv2Client

--- MasterServer/bin/init.sh
+++ MasterServer/bin/init.sh
@@ -4,7 +4,7 @@ export LD_LIBRARY_PATH="/usr/lib64/:$LD_LIBRARY_PATH"
 export LD_LIBRARY_PATH="/usr/local/lib64/:$LD_LIBRARY_PATH"
 
 # libstdc++.so.6 need GLIBCXX_3.4.30
-export LD_LIBRARY_PATH="/home/bjy/FaceRecSysOnnx/MasterServer/bin/:$LD_LIBRARY_PATH" 
+# export LD_LIBRARY_PATH="/home/bjy/FaceRecSysOnnx/MasterServer/bin/:$LD_LIBRARY_PATH" 
 # iniparser
 export LD_LIBRARY_PATH="../thirdparty/iniparser/lib/:$LD_LIBRARY_PATH"
 # mysqlclient
